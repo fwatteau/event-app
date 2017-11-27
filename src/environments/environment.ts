a95747814d1fdec4e3fc932a0279bab87ca98777
Franck WATTEAU <f.watteau@gmail.com>
2017-11-27 21:44:43 +0100

    Update and rename environment.prod.ts to environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "your_api_key",
+    authDomain: "your_auth_domain",
+    databaseURL: "your_database_url",
+    projectId: "your_id",
+    storageBucket: "your_id",
+    messagingSenderId: "your_other_id"
+  }
+};
